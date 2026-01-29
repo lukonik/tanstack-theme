3db74b5be249cf0fdee75eeea24d25880f323f97
luka <lukaonik@gmail.com>
2026-01-29 11:20:28 +0400

    add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 /// <reference types="vitest/config" />
-import react from '@vitejs/plugin-react'
-import { playwright } from '@vitest/browser-playwright'
-import { defineConfig } from 'vite'
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: './playground',
-  plugins: [react()],
+  root: "./playground",
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   test: {
-    root: '.',
+    root: ".",
     browser: {
       enabled: true,
       provider: playwright(),
-      instances: [{ browser: 'chromium' }],
+      instances: [{ browser: "chromium" }],
       headless: true,
     },
   },
-})
+});
